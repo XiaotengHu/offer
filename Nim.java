a4982e6d037bd83244d49b8c81f50046afc00a16
xiaotenghu@tju.edu.cn <xiaotenghu@tju.edu.cn>
2018-04-02 16:06:27 +0800

    first commit

---
+++ Nim.java
@@ -0,0 +1,15 @@
+package work.test;
+
+public class Nim {
+//	public static int nim(int x, int y){
+//		double a = (Math.sqrt(5.0) + 1)/2;
+//		int n = Math.abs(x-y);
+//		x = x< y? x:y;
+////		x != (int)a*n;
+////		return (x != int(a*n));
+//	}
+public static void main(String[] args) {
+	int x, y;
+	
+}
+}
